3b951ac4541e28c66ed9775035f52736736e6086
valentelucass <lucasmac.dev@gmail.com>
2026-05-07 18:14:12 -0300

    feat: adicionar campo cliente_cnpj e ajustar conversões de data na view de faturas

--- database/views/011_criar_view_faturas_por_cliente_powerbi.sql
+++ database/views/011_criar_view_faturas_por_cliente_powerbi.sql
@@ -1,7 +1,14 @@
+IF OBJECT_ID(N'dbo.faturas_por_cliente', N'U') IS NOT NULL
+   AND COL_LENGTH('dbo.faturas_por_cliente', 'cliente_cnpj') IS NULL
+BEGIN
+    ALTER TABLE dbo.faturas_por_cliente ADD cliente_cnpj NVARCHAR(14) NULL;
+END;
+GO
+
 CREATE OR ALTER VIEW dbo.vw_faturas_por_cliente_powerbi AS
 SELECT
 
-    CAST(fpc.data_emissao_cte AS TIME(0)) AS [Hora (Solicitacao)],
+    CAST(datas.data_emissao_cte AS TIME(0)) AS [Hora (Solicitacao)],
     fpc.unique_id AS [ID Único],
     fpc.filial AS [Filial],
     fpc.estado AS [Estado],
@@ -12,7 +19,7 @@ SELECT
         ELSE NULL
     END AS [Número do Documento],
     fpc.chave_cte AS [CT-e/Chave],
-    fpc.data_emissao_cte AS [CT-e/Data de emissão],
+    datas.data_emissao_cte AS [CT-e/Data de emissão],
     fpc.valor_frete AS [Frete/Valor dos CT-es],
     fpc.third_party_ctes_value AS [Terceiros/Valor CT-es],
     fpc.status_cte AS [CT-e/Status],
@@ -21,7 +28,7 @@ SELECT
     fpc.classificacao AS [Classificação],
     fpc.pagador_nome AS [Pagador do frete/Nome],
     fpc.pagador_documento AS [Pagador do frete/Documento],
-    fpc.cliente_cnpj AS [Cliente/CNPJ],
+    COALESCE(fpc.cliente_cnpj, documentos.pagador_cnpj) AS [Cliente/CNPJ],
     fpc.remetente_nome AS [Remetente/Nome],
     fpc.remetente_documento AS [Remetente/Documento],
     fpc.destinatario_nome AS [Destinatário/Nome],
@@ -35,19 +42,42 @@ SELECT
     fg.instrucao_boleto AS [Instrução Customizada],
     CASE WHEN fpc.fit_ant_document IS NOT NULL THEN 'Faturado' ELSE 'Aguardando Faturamento' END AS [Status do Processo],
     fpc.fit_ant_document AS [Fatura/N° Documento],
-    fpc.fit_ant_issue_date AS [Fatura/Emissão],
+    CONVERT(NVARCHAR(10), datas.fit_ant_issue_date, 23) AS [Fatura/Emissão],
     fpc.fit_ant_value AS [Fatura/Valor],
     fpc.valor_fatura AS [Fatura/Valor Total],
     fpc.numero_fatura AS [Fatura/Número],
-    fpc.data_emissao_fatura AS [Fatura/Emissão Fatura],
-    fpc.data_vencimento_fatura AS [Parcelas/Vencimento],
-    fpc.data_baixa_fatura AS [Fatura/Baixa],
-    fpc.fit_ant_ils_original_due_date AS [Fatura/Data Vencimento Original],
+    CONVERT(NVARCHAR(10), datas.data_emissao_fatura, 23) AS [Fatura/Emissão Fatura],
+    CONVERT(NVARCHAR(10), datas.data_vencimento_fatura, 23) AS [Parcelas/Vencimento],
+    CONVERT(NVARCHAR(10), datas.data_baixa_fatura, 23) AS [Fatura/Baixa],
+    CONVERT(NVARCHAR(10), datas.fit_ant_ils_original_due_date, 23) AS [Fatura/Data Vencimento Original],
     fpc.notas_fiscais AS [Notas Fiscais],
     fpc.pedidos_cliente AS [Pedidos/Cliente],
     fpc.metadata AS [Metadata],
     fpc.data_extracao AS [Data da Última Atualização]
 FROM dbo.faturas_por_cliente fpc
+OUTER APPLY (
+    SELECT
+        TRY_CONVERT(DATETIMEOFFSET, fpc.data_emissao_cte) AS data_emissao_cte,
+        TRY_CONVERT(DATE, fpc.fit_ant_issue_date) AS fit_ant_issue_date,
+        TRY_CONVERT(DATE, fpc.data_emissao_fatura) AS data_emissao_fatura,
+        TRY_CONVERT(DATE, fpc.data_vencimento_fatura) AS data_vencimento_fatura,
+        TRY_CONVERT(DATE, fpc.data_baixa_fatura) AS data_baixa_fatura,
+        TRY_CONVERT(DATE, fpc.fit_ant_ils_original_due_date) AS fit_ant_ils_original_due_date
+) datas
+OUTER APPLY (
+    SELECT
+        CASE
+            WHEN documento_limpo NOT LIKE '%[^0-9]%'
+             AND LEN(documento_limpo) = 14
+            THEN documento_limpo
+            ELSE NULL
+        END AS pagador_cnpj
+    FROM (
+        SELECT REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+            LTRIM(RTRIM(COALESCE(CONVERT(NVARCHAR(50), fpc.pagador_documento), N''))),
+            N'.', N''), N'-', N''), N'/', N''), N' ', N''), CHAR(9), N''), CHAR(160), N'') AS documento_limpo
+    ) limpeza
+) documentos
 OUTER APPLY (
     -- Mantem o enriquecimento 1:1 e evita multiplicar faturas quando ha varios titulos com o mesmo document.
     SELECT TOP (1)
@@ -58,16 +88,16 @@ OUTER APPLY (
     WHERE fg.document = fpc.fit_ant_document
     ORDER BY
         CASE
-            WHEN fpc.fit_ant_issue_date IS NOT NULL AND fg.issue_date = fpc.fit_ant_issue_date THEN 0
-            WHEN fpc.fit_ant_issue_date IS NULL THEN 1
+            WHEN datas.fit_ant_issue_date IS NOT NULL AND TRY_CONVERT(DATE, fg.issue_date) = datas.fit_ant_issue_date THEN 0
+            WHEN datas.fit_ant_issue_date IS NULL THEN 1
             ELSE 2
         END,
         CASE
-            WHEN fpc.fit_ant_issue_date IS NOT NULL AND fg.issue_date IS NOT NULL
-                THEN ABS(DATEDIFF(DAY, fg.issue_date, fpc.fit_ant_issue_date))
+            WHEN datas.fit_ant_issue_date IS NOT NULL AND TRY_CONVERT(DATE, fg.issue_date) IS NOT NULL
+                THEN ABS(DATEDIFF(DAY, TRY_CONVERT(DATE, fg.issue_date), datas.fit_ant_issue_date))
             ELSE 2147483647
         END,
-        fg.issue_date DESC,
+        TRY_CONVERT(DATE, fg.issue_date) DESC,
         fg.id DESC
 ) fg;
 GO
